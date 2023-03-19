af6b068822092527088d5bf4ec8647959c9eb565
BartoszewskiA <bartoszewski.a@outlook.com>
2023-03-19 09:31:22 +0100

    19.03.2023

--- lab01p02.cpp
+++ lab01p02.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+int ile = 0;
 
 class Osoba
 {
@@ -14,12 +16,90 @@ public:
     void setImie(string im) { imie = im; }
     void setNazwisko(string nazw) { nazwisko = nazw; }
     int getId() { return id; }
-    string getimie() { return imie; }
+    string getImie() { return imie; }
     string getNazwisko() { return nazwisko; }
+
+    void wczytajDane();
+    void wypiszOsobe();
 };
 
+void Osoba::wczytajDane()
+{
+    id = ile + 1;
+    cout << "Podaj imie: ";
+    getline(cin, imie);
+    cout << "Podaj nazwisko: ";
+    getline(cin, nazwisko);
+}
+
+void Osoba::wypiszOsobe()
+{
+    cout << "\n-----------------------------\n"
+         << "ID:\t\t" << id
+         << "\nImie:\t\t" << imie
+         << "\nNazwisko:\t" << nazwisko
+         << "\n-----------------------------\n";
+}
+
+Osoba *baza[1000];
+
+void wypiszBaze();
+void dodajOsobe();
+void usunBaze();
 int main()
 {
+    int wybor = 0;
+    system("clr");
+    cout << "1 - Dodaj osobe" << endl
+         << "2 - Wypisz baze" << endl
+         << "3 - eksportuj do pliku" << endl
+         << "4 - importuj z pliku" << endl
+         << "8 - usun baze" << endl
+         << "9 - wyjdz z programu";
+    cin >> wybor;
+    cin.clear();
+    cin.ignore(10, '\n'); // czyszczenie bufor pamięci
 
+    switch (wybor)
+    {
+    case 1:
+        dodajOsobe();
+        break;
+    case 2:
+        wypiszBaze();
+        break;
+    case 3:
+        cout << "Kup wersje premium\n";
+        break;
+    case 4:
+        cout << "Kup wersje premium\n";
+        break;
+    case 8:
+        usunBaze();
+        break;
+    case 9:
+        usunBaze();
+        return 0;
+    }
     return 0;
+}
+
+void wypiszBaze()
+{
+    for (int i = 0; i < ile; i++)
+        baza[i]->wypiszOsobe();
+}
+
+void dodajOsobe()
+{
+    baza[ile] = new Osoba;
+    baza[ile]->wczytajDane();
+    ile++;
+}
+
+void usunBaze()
+{
+    for (int i = 0; i < ile; i++)
+        delete baza[i];
+    ile = 0;
 }
